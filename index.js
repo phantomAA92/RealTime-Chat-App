6f9db7afe210ba7338ea6e2ddeec85531742fe59
phantomAA92 <phantomAA92@gmail.com>
2025-08-03 11:28:30 -0700

    update for vercel test

--- index.js
+++ index.js
@@ -51,10 +51,11 @@ app.use(express.json());
 app.use(express.static('public'));
 
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
+  // cors: {
+  //   origin: "http://localhost:3000",
+  //   methods: ["GET", "POST"]
+  // }
+  cors: {}
 });
 
 const JWT_SECRET = 'your_jwt_secret_key';
